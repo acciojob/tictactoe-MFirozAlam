385dc500df3b00b16a3583effb3a522aeb7d5d83
MFirozAlam <146247719+MFirozAlam@users.noreply.github.com>
2026-05-22 12:00:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const submitBtn = document.getElementById("submit");
 const startScreen = document.getElementById("start-screen");
 const gameScreen = document.getElementById("game-screen");
 
-const player1Input = document.getElementById("player-1");
-const player2Input = document.getElementById("player-2");
+const player1Input = document.getElementById("player1");
+const player2Input = document.getElementById("player2");
 
 const message = document.querySelector(".message");
 const cells = document.querySelectorAll(".cell");
